cb6b537e1d5cfaf9d3869099fe984fbfa8530671
AbrahamOdoi <abrahamodoi2002@gmail.com>
2014-02-06 13:47:18 +0000

    w
    w

---
+++ books.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+
+<html>
+	<title>ICGC Devotional</title>
+	<head>
+
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<!-- <script type="text/javascript" src="script.js"></script> -->
+		<!-- <script type="text/javascript" src="http://216.224.161.207/icgc/script.js"></script> -->
+
+		<link rel="stylesheet" href="css/style.css" />
+		<!-- <link rel="stylesheet" href="css/jquery.mobile-1.2.0.min.css" />
+		<script src="js/jquery-1.8.2.min.js"></script>
+		<script src="js/jquery.mobile-1.2.0.min.js"></script> -->
+
+		<link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+		<script type="text/javascript" src="jquery.mobile-1.0.1.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery-1.9.0.min.js"></script>
+		<script type="text/javascript" src="jquery.nivo.slider.js"></script>
+
+	</head>
+	<body>
+		<div id='quotes' data-role='page' >
+
+			<div id='header' data-role='header' >
+				<div class="div_header">
+					QUOTES
+				</div>
+				<div class="wrapper1">
+					<a href="mainpage.php" data-transition='flip'rel="external" class="span_menu" style="color: black; border-left: 1px solid gray;"><img src="icons/home.png" />
+					<br/>
+					HOME</a>
+					<a href="daily.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/devotion.png" />
+					<br/>
+					Daily</a>
+					<a href="quotes.php" data-transition='flip' class="span_menu" style="color: black; border-left: 1px solid gray;"><img src="icons/quote.png" />
+					<br/>
+					Quotes</a>
+					<a href="sermon.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/sermon.png" />
+					<br/>
+					Sermon</a>
+					<a href="books.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/books.png" />
+					<br/>
+					Books</a>
+					<a href="videos.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/videos.png" />
+					<br/>
+					Videos</a>
+					<a href="loc.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/loc.png" />
+					<br/>
+					Near You</a>
+					<a href="more.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/more.png" />
+					<br/>
+					Donate</a>
+				</div>
+
+			</div>
+
+			<div id='content' data-role='content'>
+				<!-- <p style='vertical-align:center;color: gray;'>Dr. Otabil's quotes<img src="img/2_1.jpg" class="img_quote" style="float: left;"/></p> -->
+
+				<div class="p_quotes">
+					<a href="https://www.mensaotabil.com/products-page/ebooks/goal-setting-and-goal-getting/"><img src="img/books/GoalSetting.png" class="img_quote" style="float: left;"/></a>
+					<span class="date"><a href="https://www.mensaotabil.com/products-page/ebooks/goal-setting-and-goal-getting/">Setting and Goal Getting</a></span>
+					<br/>
+					Price: $6.00
+					<br/>
+					With no clear sense of where we are going, we are bound to settle for any destination. Without vision, we easily accept anything that comes our way and allow other people to set the agenda for our lives. That is why we ned to set goals and go on to achieve them. Setting goals requires that you learn from your past experiences, locate your current opportunities and launch boldly into your future.
+					<br />
+					<br />
+					Comment prendre des décisions éclairées présente comme approche systématique et pourtant simple qui comporte six étapes clés à recevoir de bons conseils et de prendre des décisions de qualité qui vous aideront à progresser dans la vie.
+				</div>
+				<p class="p_quotes">
+					<a href="https://www.mensaotabil.com/products-page/ebooks/how-to-make-wise-decisions/"><img src="img/books/Howtomake.png" class="img_quote" style="float: left;"/></a>
+					<span class="date"><a href="https://www.mensaotabil.com/products-page/ebooks/how-to-make-wise-decisions/">How to Make Wise Decisions</a></span>
+					<br/>
+					Price: $2.00
+					<br/>
+					Receiving good advice is an art that can be perfected by practice. The quality of our lives is influenced by the people from whom we receive advice. For each one of us, there are people who enrich our lives and others who deplete our lives because of the quality of advice they give to us.
+					How to Make Wise Decisions presents as systematic and yet simple approach that involves six key steps to receiving good advice and making quality decisions which will help you advance in life.....
+					<br/>
+					<br/>
+					En l'absence de vision claire de l'endroit où nous allons, nous sommes tenus de se contenter de n'importe quelle destination. Sans vision, nous acceptons facilement tout ce qui vient à notre rencontre et permettre aux autres de définir l'ordre du jour de nos vies. C'est pourquoi nous Ned se fixer des objectifs et de passer à leur réalisation. Fixer des objectifs exige que vous apprenez de vos expériences passées, retrouver vos possibilités actuelles et de lancer hardiment dans votre avenir
+					.....
+				</p>
+
+			</div>
+
+			<div id='footer' data-role='footer'>
+
+			</div>
+		</div>
+
+	</body>
+</html>
+<script type="text/javascript" >
+	// $(document).ready(function() {
+	//
+	// $('#slider').nivoSlider();
+	//
+	// });
+</script>
---
+++ quotes.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+
+<html>
+	<title>ICGC Devotional</title>
+	<head>
+
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<!-- <script type="text/javascript" src="script.js"></script> -->
+		<!-- <script type="text/javascript" src="http://216.224.161.207/icgc/script.js"></script> -->
+
+		<link rel="stylesheet" href="css/style.css" />
+		<!-- <link rel="stylesheet" href="css/jquery.mobile-1.2.0.min.css" />
+		<script src="js/jquery-1.8.2.min.js"></script>
+		<script src="js/jquery.mobile-1.2.0.min.js"></script> -->
+
+		<link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+		<script type="text/javascript" src="jquery.mobile-1.0.1.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery-1.9.0.min.js"></script>
+		<script type="text/javascript" src="jquery.nivo.slider.js"></script>
+
+	</head>
+	<body>
+		<div id='quotes' data-role='page' >
+
+			<div id='header' data-role='header' >
+				<div class="div_header">
+					QUOTES
+				</div>
+				<div class="wrapper1">
+					<a href="mainpage.php" data-transition='flip'rel="external" class="span_menu" style="color: black; border-left: 1px solid gray;"><img src="icons/home.png" />
+					<br/>
+					HOME</a>
+					<a href="daily.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/devotion.png" />
+					<br/>
+					Daily</a>
+					<a href="quotes.php" data-transition='flip' class="span_menu" style="color: black; border-left: 1px solid gray;"><img src="icons/quote.png" />
+					<br/>
+					Quotes</a>
+					<a href="sermon.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/sermon.png" />
+					<br/>
+					Sermon</a>
+					<a href="books.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/books.png" />
+					<br/>
+					Books</a>
+					<a href="videos.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/videos.png" />
+					<br/>
+					Videos</a>
+					<a href="loc.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/loc.png" />
+					<br/>
+					Near You</a>
+					<a href="more.php" data-transition='slide' class="span_menu" style="color: black;"><img src="icons/more.png" />
+					<br/>
+					Donate</a>
+				</div>
+
+			</div>
+
+			<div id='content' data-role='content'>
+				<p style='vertical-align:center;color: gray;'>Dr. Otabil's quotes<img src="img/2_1.jpg" class="img_quote" style="float: left;"/></p>
+				<p class="p_quotes">
+					Delays are not denials. Waiting is not wasting. Though a vision tarries, wait for it. 
+					It shall come to pass.
+				</p>
+				<p class="p_quotes">
+					Every question has an answer. Every problem has a solution. Every journey has a path. 
+					Keep asking...it shall be given to you. Keep seeking...you will find. Keep knocking...it will open.
+				</p>
+				<p class="p_quotes">
+					There's no scarcity in God's economy
+				</p>
+				<p class="p_quotes">
+					USE WHAT YOU HAVE. Don't sit and hope for what others have. David had five stones; it brought down Goliath. 
+					A little boy had five loaves; it fed five thousand. Don't cry over the little in your hands. Work with it. 
+					Put it in God's hands. He can grow your tiny seeds into a mighty forest.
+				</p>
+				<p class="p_quotes">
+					Our spiritual and mental capacity determines how much we can receive from God. It's time to STRETCH spiritually and 
+					mentally. No laziness. No excuses. No complacency. STRETCH. Push. Perspire. Persevere.
+				</p>
+				<p class="p_quotes">
+					TIME IS PRECIOUS. What has to be done today must not be done tomorrow. REDEEM THE TIME.
+				</p>
+			</div>
+
+			<div id='footer' data-role='footer'>
+
+			</div>
+		</div>
+
+	</body>
+</html>
+<script type="text/javascript" >
+	// $(document).ready(function() {
+	//
+	// $('#slider').nivoSlider();
+	//
+	// });
+</script>
